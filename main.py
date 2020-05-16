65091565cdcc9222b700ece9bfadf370d87dfa7d
grazh <ballistics32@mail.ru>
2020-05-16 23:26:07 +0300

    first steps

--- main.py
+++ main.py
@@ -8,14 +8,27 @@ import json
 
 
 token = "374eeed4f9510e8e6c2e5fbfbaab5f93c8068af27a245c2f729583018f34d608e7d740e2d349cf2d28997"
+
+
+
 vk_session = vk_api.VkApi(token=token)
+vk_session1 = vk_api.VkApi('79611019237', 'fdf42fdf42')
+vk_session1.auth()
+
 longpoll = VkLongPoll(vk_session)
 flag = 0
 seminar = 0
 task = 0
 
-all_tasks = {'1': {}, '2': {}, '3': {}, '4': {}, '5': {}, '6': {}, '7': {}, '8': {}, '9': {}, '10': {},
-             'length': {'1': 21, '2': 13, '3': 34, '4': 17, '5': 30, '6': 19, '7': 20, '8': 19, '9': 16, '10': 12}}
+# lol = json()
+
+with open("all_tasks.json") as f:
+    all_tasks = json.load(f)
+
+def write_in_file(data, filename):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=2)
+
 affirmative = ["да", "верно", "правильно", "ес", "yes", "lf"]
 
 def analize_message(event, all_tasks):
@@ -30,12 +43,12 @@ def analize_message(event, all_tasks):
         try:
             int(words[1])
             int(words[3])
-            if int(words[1]) > 10:
+            if int(words[1]) > 10 or int(words[1]) < 1:
                 vk_session.method("messages.send", {'user_id': event.user_id,
                                                     'message': "Cеминара " + str(words[1]) + " пока нет",
                                                     'random_id': 0})
                 return (0, 0)
-            if int(words[3]) > all_tasks['length'][words[1]]:
+            if int(words[3]) > all_tasks['length'][words[1]] or int(words[3]) < 1:
                 vk_session.method("messages.send", {'user_id': event.user_id,
                                                 'message': "В семинаре " + str(words[1]) +
                                                            " нет задачи с номером " + str(words[3]) + "!",
@@ -50,12 +63,12 @@ def analize_message(event, all_tasks):
         try:
             int(words[0])
             int(words[1])
-            if int(words[0]) > 10:
+            if int(words[0]) > 10 or int(words[0]) < 1:
                 vk_session.method("messages.send", {'user_id': event.user_id,
                                                     'message': "Cеминара " + str(words[0]) + " пока нет",
                                                     'random_id': 0})
                 return (0, 0)
-            if int(words[1]) > all_tasks['length'][words[0]]:
+            if int(words[1]) > all_tasks['length'][words[0]] or int(words[1]) < 1:
                 vk_session.method("messages.send", {'user_id': event.user_id,
                                                 'message': "В семинаре " + str(words[0]) +
                                                            " нет задачи с номером " + str(words[1]) + "!",
@@ -70,12 +83,13 @@ def analize_message(event, all_tasks):
 
 def get_photo(seminar, task, all_tasks):
     try:
-        if '-' in all_tasks[seminar][task]:
+        if '.com' in all_tasks[seminar][task]:
             print("Here")
+            print(seminar)
+            print(task)
             vk_session.method("messages.send", {'user_id': event.user_id,
-                                                'message': "",
-                                                'random_id': 0,
-                                                'attachment': "photo" + all_tasks[seminar][task]})
+                                                'message': "Решение задания находится по ссылке:\n" + all_tasks[seminar][task],
+                                                'random_id': 0})
             return 1
     except:
         vk_session.method("messages.send", {'user_id': event.user_id,
@@ -87,29 +101,6 @@ def get_photo(seminar, task, all_tasks):
                                             'random_id': 0})
         return 0
 
-# def write_in_file(data, filename):
-#     with open(filename, 'w') as file:
-#         json.dump(data, file)
-
-def ask_to_upload(event, seminar, task):
-    if event.text in affirmative:
-        vk_session.method("messages.send", {'user_id': event.user_id,
-                                            'message': "Отправьте сообщение, прикрепив к ней фото с решением задачи",
-                                            'random_id': 0})
-        return 1
-    else:
-        vk_session.method("messages.send", {'user_id': event.user_id,
-                                            'message': "Попросим других пользователей добавить ответ на задачу",
-                                            'random_id': 0})
-        print("fuu")
-        # просит пользователей из списка прислать решение задачи
-        return 0
-
-x = 1
-
-def write_in_file(data, filename):
-    with open(filename, "w") as file:
-        json.dump(data, filename)
 
 def remember_users(user_id):
     try:
@@ -119,41 +110,35 @@ def remember_users(user_id):
     except:
         write_in_file(user_id, "user_id")
 
+def analize_request(event, seminar, task, all_tasks):
+    if seminar == 0:
+        return 0
+    else:
+        if "attach1" in event.attachments.keys():
+            all_tasks[seminar][task] = vk_session.method("messages.getHistoryAttachments", {'peer_id': event.user_id,
+                                                    "media_type": "photo"})['items'][0]['attachment']['photo']['sizes'][-1]['url']
+            write_in_file(all_tasks, "all_tasks.json")
+            vk_session.method("messages.send", {'user_id': event.user_id,
+                                                'message': "Спасибо, что добавили решение задачи " + str(task) + " из семинара " + str(seminar) + "!",
+                                                'random_id': 0})
+        else:
+            if task in all_tasks[seminar].keys() and ".com" in all_tasks[seminar][task]:
+                vk_session.method("messages.send", {'user_id': event.user_id,
+                                                    'message': "Решение задачи находится по ссылке\n" + all_tasks[seminar][task],
+                                                    'random_id': 0})
+            else:
+                vk_session.method("messages.send", {'user_id': event.user_id,
+                                                    'message': "Решение задачи еще не выложили",
+                                                    'random_id': 0})
+        print(event.attachments)
+
 
 while True:
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW:
             # remember_users(event.user_id)
             if not event.from_me:
-                if seminar == 0:
-                    vk_session.method("messages.send", {'user_id': event.user_id,
-                                                        'message': "1",
-                                                        'random_id': 0,
-                                                        'attachment': ['photo149640303_457243386']})
-
-                    seminar, task = analize_message(event, all_tasks)
-                    print("seminar = " + str(seminar))
-                    print("task = " + str(task))
-                if seminar != 0:
-                    if x == 0:
-                        if flag != 1 and ask_to_upload(event, seminar, task) == 0:
-                            x = 1
-                            seminar = 0
-                        else:
-                            if flag == 1:
-                                all_tasks[seminar][task] = event.attachments['attach1']
-                                print(event.attachments)
-                                attach = "photo" + str(all_tasks[seminar][task])
-                                print(attach)
-                                vk_session.method("messages.send", {'user_id': event.user_id,
-                                                                    'message': "1",
-                                                                    'random_id': 11,
-                                                                    'attachment': [str(attach)]})
-                                print(all_tasks)
-                                flag = 0
-                                x = 1
-                                seminar = 0
-                            else:
-                                flag= 1
-                    else:
-                        x = get_photo(seminar, task, all_tasks)
+                seminar, task = analize_message(event, all_tasks)
+                analize_request(event, seminar, task, all_tasks)
+                print("seminar = " + str(seminar))
+                print("task = " + str(task))

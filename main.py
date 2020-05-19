6c11506cd73f850304039795698740323325c9e1
grazh <ballistics32@mail.ru>
2020-05-19 19:35:59 +0300

    alpha +

--- main.py
+++ main.py
@@ -26,29 +26,16 @@ def response(event, text):
     vk_session.method("messages.send", {'user_id': event.user_id, 'message': text, 'random_id': 0})
 
 def analize_message(event, all_tasks):
-    if len(event.text.split()) == 4 or len(event.text.split()) == 2:
+    if len(event.text.split()) == 2:
         words = event.text.split()
     else:
-        response(event, "Запрос не соответствует формату.\nВведите Семинар _ задача _ или просто 2 цифры")
+        response(event, "Запрос не соответствует формату.\n🚀 Введите через пробел номер семинара и номер задачи из него (например, '2 5' выдаст решение пятой задачи из второго семинара)")
         return (0, 0)
-    if len(event.text.split()) == 4:
-        try:
-            int(words[1])
-            int(words[3])
-            if int(words[1]) > 10 or int(words[1]) < 1:
-                response(event, "Cеминара " + str(words[1]) + " пока нет")
-                return (0, 0)
-            if int(words[3]) > all_tasks['length'][words[1]] or int(words[3]) < 1:
-                response(event, "В семинаре " + str(words[1]) + " нет задачи с номером " + str(words[3]) + "!")
-                return (0, 0)
-            return (words[1], words[3])
-        except:
-            response(event, "Введите числовое значение семинара и задачи по формату\nСеминар _ задача _ или просто 2 цифры")
-    elif len(event.text.split()) == 2:
+    if len(event.text.split()) == 2:
         try:
             int(words[0])
             int(words[1])
-            if int(words[0]) > 10 or int(words[0]) < 1:
+            if int(words[0]) > 12 or int(words[0]) < 1:
                 response(event, "Cеминара " + str(words[0]) + " пока нет")
                 return (0, 0)
             if int(words[1]) > all_tasks['length'][words[0]] or int(words[1]) < 1:
@@ -56,16 +43,22 @@ def analize_message(event, all_tasks):
                 return (0, 0)
             return (words[0], words[1])
         except:
-            response(event, "Введите числовое значение семинара и задачи по формату\nСеминар _ задача _ или просто 2 цифры")
+            response(event, "Запрос не соответствует формату.\n🚀 Введите через пробел номер семинара и номер задачи из него (например, '2 5' выдаст решение пятой задачи из второго семинара)")
     return (0, 0)
 
 def remember_users(user_id):
     with open('user_id', "r") as f:
         if str(user_id) not in f.read():
-            with open('user_id', "w") as file:
+            x = 1
+        else:
+            x = 0
+    if x == 1:
+        with open('user_id', "a+") as file:
                 file.write(str(user_id))
                 file.write('\n')
                 return 0
+    else:
+        return 1
 
 def analize_request(event, seminar, task, all_tasks):
     if seminar == 0:
@@ -73,23 +66,36 @@ def analize_request(event, seminar, task, all_tasks):
     else:
         if "attach1" in event.attachments.keys():
             for i in range(1, int(len(str(event.attachments.keys()).split())/2) + 1):
-                if event.attachments['attach' + str(i) + "_type"] != "doc" and event.attachments['attach' + str(i) + "_type"] != "photo":
-                    response(event, "Решением задачи может быть только фото или документ")
+                if event.attachments['attach' + str(i) + "_type"] != "photo":
+                    response(event, "Решением задачи может быть только фото")
                     return 0
+            r = vk_session.method("messages.getById", {"message_ids": event.message_id})
+            access_key = []
             if task in all_tasks[seminar].keys() and "0" in all_tasks[seminar][task].keys() and "photo" in all_tasks[seminar][task]['0']:
                 response(event, "Задача " + str(task) + " из семинара " + str(seminar) + " уже решена")
-                vk_session.method("messages.send", {'user_id': event.user_id,
-                                                    'message': "Наберите: " + str(seminar) + " " + str(task),
-                                                    'random_id': 0})
+                response(event, "Если вы хотите заменить существующее решение, напишите @saturnnm (Мне) в лс")
                 return 0
-            r = vk_session.method("messages.getById", {"message_ids": event.message_id})
-            access_key = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-            all_tasks[seminar][task] = {}
+            else:
+                all_tasks[seminar][task] = {}
             for i in range(0, int(len(str(event.attachments.keys()).split())/2)):
-                access_key[i] = r['items'][0]['attachments'][i]['photo']['access_key']
+                access_key.append(r['items'][0]['attachments'][i]['photo']['access_key'])
                 string = "photo" + str(event.attachments["attach" + str(i+1)]) + "_" + str(access_key[i])
                 all_tasks[seminar][task][str(i)] = string
-            write_in_file(all_tasks, "all_tasks.json")
+            with open("moderators_ids", "r") as f:
+                moderators = f.read()
+                if str(event.user_id) in moderators:
+                    write_in_file(all_tasks, "all_tasks.json")
+                else:
+                    attach = str(all_tasks[seminar][task]['0'])
+                    for i in range(1, all_tasks[seminar][task].keys().__len__()):
+                        attach += "," + str(all_tasks[seminar][task][str(i)])
+                    for j in moderators.split('\n'):
+                        print(j)
+                        vk_session.method("messages.send", {'user_id': j,
+                                                            'message':  seminar + " " + task,
+                                                            'random_id': 0,
+                                                            'attachment': attach})
+                    delete_image(event.text, all_tasks)
             response(event,  "Спасибо, что добавили решение задачи " + str(task) + " из семинара " + str(seminar) + "!")
         else:
             if task in all_tasks[seminar].keys() and '0' in all_tasks[seminar][task].keys() and "photo" in all_tasks[seminar][task]['0']:
@@ -97,32 +103,31 @@ def analize_request(event, seminar, task, all_tasks):
                 for i in range(1, all_tasks[seminar][task].keys().__len__()):
                     attach += "," + str(all_tasks[seminar][task][str(i)])
                 vk_session.method("messages.send", {'user_id': event.user_id,
-                                                    'message': "Решение задачи " + task + " из семинара " + seminar + ":",
+                                                    'message': "Решение задачи " + task + " из семинара "+ seminar + ":",
                                                     'random_id': 0,
                                                     'attachment': attach})
 
             else:
-                response(event, "Решение задачи еще не выложили 😒")
+                response(event, "Решение задачи еще не выложили 🐨")
             return 0
 
 def ask_help(event):
-    response(event, "Чтобы получить решение задачи, введите\n'Семинар _ задача _' или просто две цифры, соответствующие номеру лекции(семинара) и задачи.\n\n"
-        + "Чтобы добавить свое решение, отправьте сообщение с номером лекции(семинара) и задачи и прикрепите к нему фото с решением.\n\n"
-        + "Чтобы увидеть список всех добавленных задач напишите 'все'.\n\n"
-        + "Чтобы увидеть подсказку еще раз, напишите 'help'.\n\n"
+    response(event, "Подсказака:\n\n" + "⚡ Чтобы получить решение задачи, введите через пробел номер семинара и номер задачи из него (например, '2 5' выведет решение пятой задачи из второго семинара)\n\n"
+        + "⚡ Добавить свое решение можно прикрепив фото к сообщению с номером семинара и задачи.\n\n"
+        + "⚡ Команда 'все' выведет список всех добавленных задач.\n\n"
+        + "⚡ Вызвать подсказку еще раз можно командой 'хелп'.\n\n"
         + "По другим вопросам пишите @saturnnm (Мне) 😉")
 
 def delete_image(text, all_tasks):
     words = text.split()
-    if words[2] in all_tasks[words[1]].keys():
-        tmp = all_tasks[words[1]][words[2]].keys()
-        if "0" in all_tasks[words[1]][words[2]].keys():
-            del all_tasks[words[1]][words[2]]['0']
-        if "1" in all_tasks[words[1]][words[2]].keys():
-            del all_tasks[words[1]][words[2]]['1']
-        if "2" in all_tasks[words[1]][words[2]].keys():
-            del all_tasks[words[1]][words[2]]['2']
-        del all_tasks[words[1]][words[2]]
+    if words[1] in all_tasks[words[0]].keys():
+        if "0" in all_tasks[words[0]][words[1]].keys():
+            del all_tasks[words[0]][words[1]]['0']
+        if "1" in all_tasks[words[0]][words[1]].keys():
+            del all_tasks[words[0]][words[1]]['1']
+        if "2" in all_tasks[words[0]][words[1]].keys():
+            del all_tasks[words[0]][words[1]]['2']
+        del all_tasks[words[0]][words[1]]
         write_in_file(all_tasks, "all_tasks.json")
         return all_tasks
 
@@ -135,27 +140,52 @@ def print_all_tasks(all_tasks, event):
             for task in (all_tasks[seminar].keys()):
                 tasks_array.append(int(task))
             tasks_array = sorted(tasks_array)
-            for task1 in sorted(tasks_array):
-                string += str(task1) + ", "
-            string = string[0:-2] + "\n"
-    vk_session.method("messages.send", {"user_id": event.user_id, "message": string, "random_id": 0})
+            for i in range(0, len(tasks_array)):
+                tasks_array[i] = str(tasks_array[i])
+            string = string + ', '.join(tasks_array) + "\n"
+    response(event, string)
 
-def main(all_tasks, seminar):
+def main(all_tasks):
     x = 1
-    previous_url = 0
     try:
         while x == 1:
             for event in longpoll.listen():
                 if event.type == vk_api.longpoll.VkEventType.MESSAGE_NEW:
                     if not event.from_me:
-                        if event.text == "help" or seminar == 'first':
-                            if remember_users(event.user_id) == 0:
+                        print(event.text)
+                        c = remember_users(event.user_id)
+                        if event.text == "хелп" or c == 0:
+                            if c == 0:
                                 response(event, "Приветствую тебя!")
                             ask_help(event)
                             seminar = 0
+                        elif event.text == "error":
+                            print(lol)
                         elif event.text == "stop23012001":
                             x = 0
                             break
+                        elif "reply" in event.attachments.keys():
+                                with open("moderators_ids", "r") as f:
+                                    if str(event.user_id) in f.read():
+                                        r = vk_session.method("messages.getById", {"message_ids": event.message_id})
+                                        words = r['items'][0]['reply_message']['text'].split()
+                                        if words[1] in all_tasks[words[0]]:
+                                            vk_session.method("messages.send", {'user_id': event.user_id,
+                                                                                'message': "Спасибо, уже добавлена",
+                                                                                'random_id': 0})
+                                        else:
+                                            boarder = len(r['items'][0]['reply_message']['attachments'])
+                                            print(words)
+                                            attach = []
+                                            if words[1] not in all_tasks[words[0]].keys():
+                                                all_tasks[words[0]][words[1]] = {}
+                                            for i in range(0, boarder):
+                                                attach.append("photo" + str(r['items'][0]['reply_message']['attachments'][i]['photo']['owner_id']) + "_" + str(r['items'][0]['reply_message']['attachments'][i]['photo']['id']) + "_" + str(r['items'][0]['reply_message']['attachments'][i]['photo']['access_key']))
+                                                all_tasks[words[0]][words[1]][str(i)] = attach[i]
+                                            write_in_file(all_tasks, "all_tasks.json")
+                                            response(event, "Добавлено, спасибо!")
+                                    else:
+                                        response(event, "Введите 'хелп', чтобы увидеть подсказку.")
                         elif "delete23012001" in event.text:
                             all_tasks = delete_image(event.text, all_tasks)
                         elif event.text == "Все" or event.text == "все":
@@ -165,7 +195,7 @@ def main(all_tasks, seminar):
                             analize_request(event, seminar, task, all_tasks)
     except:
         print("Error occured.")
-        main(all_tasks, 0)
+        main(all_tasks)
 
 if __name__ == "__main__":
-    main(all_tasks, "first")
+    main(all_tasks)
--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-import time
-
-time_sent = time.time()
-print(time_sent)
-time.sleep(5)
-time_sent1 = time.time()
-print(time_sent)
-print(time_sent1 - time_sent)
+with open("moderators_ids") as f:
+	print(f.read())
+	print(f.read())

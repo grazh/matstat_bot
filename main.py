9fca5bc570d1a81f5073a1fc01b017798aea7613
grazh <ballistics32@mail.ru>
2020-08-27 16:51:38 +0300

    1

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import vk_api.longpoll
+import os
 import time
 import json
+import vk_api.longpoll
+from decouple import config
 from group_digits import group_digits
-from group_token import group_token
+from urllib.request import urlretrieve
 
-vk_session = vk_api.VkApi(token=group_token)
-longpoll = vk_api.longpoll.VkLongPoll(vk_session)
+vk_group_session = vk_api.VkApi(token=config('VK_TEST_GROUP_TOKEN'))
+longpoll = vk_api.longpoll.VkLongPoll(vk_group_session)
+
+login = config('VK_LOGIN')
+password = config('VK_PASSWORD')
+album_id, group_id = config('ALBUM_ID'), config('GROUP_ID')
+
+vk_private_session = vk_api.VkApi(login=login, password=password, app_id=2685278)
+vk_private_session.auth(token_only=True)
+
+session_api = vk_private_session.get_api()
+longpoll_private = vk_api.longpoll.VkLongPoll(vk_private_session)
 
 with open("all_tasks.json") as f:
     all_tasks = json.load(f)
@@ -23,9 +35,11 @@ def response(user_id, text):
     
     """
     time.sleep(0.7)
-    vk_session.method("messages.markAsRead", {"peer_id": user_id , "v": 5.103})
+    vk_group_session.method("messages.markAsRead", {"peer_id": user_id , "v": 5.103})
     time.sleep(0.7)
-    vk_session.method("messages.send", {'user_id': user_id, 'message': text, 'random_id': 0})
+    vk_group_session.method("messages.send", {'user_id': user_id,
+                                              'message': text,
+                                              'random_id': 0})
 
 def analize_message(event, all_tasks):
     """
@@ -90,43 +104,36 @@ def analize_request(event, seminar, task, all_tasks):
             if event.attachments['attach' + str(i) + "_type"] != "photo":
                 response(event.user_id, "Решением задачи может быть только фото")
                 return 0
-        access_key = []
         if task in all_tasks[seminar].keys() and "0" in all_tasks[seminar][task].keys() and "photo" in all_tasks[seminar][task]['0']:
             response(event.user_id, "Задача " + str(task) + " из семинара " + str(seminar) + " уже решена")
             response(event.user_id, "Если вы хотите заменить существующее решение, напишите @saturnnm (Мне) в лс")
             return 0
         else:
             all_tasks[seminar][task] = {}
-        for i in range(0, int(len(str(event.attachments.keys()).split())/2)):
-            access_key.append(vk_session.method("messages.getById", {"message_ids": event.message_id})['items'][0]['attachments'][i]['photo']['access_key'])
-            string = "photo" + str(event.attachments["attach" + str(i+1)]) + "_" + str(access_key[i])
-            all_tasks[seminar][task][str(i)] = string
         with open("moderators_ids", "r") as f:
             moderators = f.read()
             if str(event.user_id) in moderators:
+                all_tasks = add_photo(event, all_tasks, seminar, task)
                 write_in_file(all_tasks, "all_tasks.json")
             else:
                 attach = str(all_tasks[seminar][task]['0'])
                 for i in range(1, all_tasks[seminar][task].keys().__len__()):
                     attach += "," + str(all_tasks[seminar][task][str(i)])
                 for j in moderators.split('\n'):
-                    vk_session.method("messages.send", {'user_id': int(j),
+                    vk_group_session.method("messages.send", {'user_id': int(j),
                                                         'message':  seminar + " " + task,
                                                         'random_id': 0,
                                                         'attachment': attach})
                 delete_image(event.text, all_tasks)
-        user = vk_session.method("users.get", {"user_ids": event.user_id})
+        user = vk_group_session.method("users.get", {"user_ids": event.user_id})
         response(event.user_id,  user[0]['first_name'] + ", спасибо, что добавили решение задачи " + str(task) + " из семинара " + str(seminar) + "!")
     else:
         if task in all_tasks[seminar].keys() and '0' in all_tasks[seminar][task].keys() and "photo" in all_tasks[seminar][task]['0']:
-            attach = str(all_tasks[seminar][task]['0'])
-            for i in range(1, all_tasks[seminar][task].keys().__len__()):
-                attach += "," + str(all_tasks[seminar][task][str(i)])
-            vk_session.method("messages.send", {'user_id': event.user_id,
-                                                'message': "Решение задачи " + task + " из семинара "+ seminar + ":",
-                                                'random_id': 0,
-                                                'attachment': attach})
-
+            attach = ','.join(all_tasks[seminar][task].values())
+            vk_group_session.method("messages.send", {'user_id': event.user_id,
+                                                      'message': "Решение задачи " + task + " из семинара " + seminar +":",
+                                                      'random_id': 0,
+                                                      'attachment': attach})
         else:
             response(event.user_id, "Решение задачи еще не выложили 🐨")
         return 0
@@ -136,7 +143,9 @@ def ask_help(user_id):
     просто выводит список правил, вызывается командой "хелп"
     
     """
-    response(user_id, "Подсказака:\n\n" + "⚡ Чтобы получить решение задачи, введите через пробел номер семинара и номер задачи из него (например, '2 5' выведет решение пятой задачи из второго семинара)\n\n"
+    response(user_id, "Подсказака:\n\n"
+        + "⚡ Чтобы получить решение задачи, введите через пробел номер семинара и номер задачи из него"
+        + "(например, '2 5' выведет решение пятой задачи из второго семинара)\n\n"
         + "⚡ Добавить свое решение можно прикрепив фото к сообщению с номером семинара и задачи.\n\n"
         + "❗ Команда 'все' выведет список всех добавленных задач.\n\n"
         + "⚡ Вызвать подсказку еще раз можно командой 'хелп'.\n\n"
@@ -174,64 +183,90 @@ def print_all_tasks(all_tasks, user_id):
 
 def accept_photo(event):
     """
-    функция, которая обеспечивает удобство жизни модераторам. в случае, если бот получает reply с любым текстом от модератора, то он добавляет вложения в all_tasks.json
+    функция, которая обеспечивает удобство жизни модераторам. в случае, если бот получает reply
+    с любым текстом от модератора, то он добавляет вложения в all_tasks.json
     если reply получен от обычного пользователя, то ему предлагают изменить запрос и его действия нигде не учитываются.
     
     """
     with open("moderators_ids", "r") as f:
         if str(event.user_id) in f.read():
-            r = vk_session.method("messages.getById", {"message_ids": event.message_id})
+            r = vk_group_session.method("messages.getById", {"message_ids": event.message_id})
             words = r['items'][0]['reply_message']['text'].split()
             if words[1] in all_tasks[words[0]]:
-                vk_session.method("messages.send", {'user_id': event.user_id,
-                                                    'message': "Спасибо, уже добавлена",
-                                                    'random_id': 0})
+                response(event.user_id, "Спасибо, уже добавлена")
             else:
-                number_of_attachs = len(r['items'][0]['reply_message']['attachments'])
-                attach = []
-                if words[1] not in all_tasks[words[0]].keys():
-                    all_tasks[words[0]][words[1]] = {}
-                    for i in range(0, number_of_attachs):
-                        attach.append("photo" + str(r['items'][0]['reply_message']['attachments'][i]['photo']['owner_id']) + "_" + str(r['items'][0]['reply_message']['attachments'][i]['photo']['id']) + "_" + str(r['items'][0]['reply_message']['attachments'][i]['photo']['access_key']))
-                        all_tasks[words[0]][words[1]][str(i)] = attach[i]
-                    write_in_file(all_tasks, "all_tasks.json")
-                    response(event.user_id, "Добавлено, спасибо!")
+                add_photo(event, all_tasks, words[0], words[1])
         else:
              response(event.user_id, "Введите 'хелп', чтобы увидеть подсказку.")
 
+
+def add_photo(event, all_tasks, seminar, task):
+    try:
+        info_photos = []
+        for k, v in event.attachments.items():
+            if v == 'photo':
+                info_photos.append(vk_group_session.method("messages.getById", {"message_ids": event.message_id, 'v': 5.77}))
+        if not os.path.exists('saved'):
+            os.mkdir('saved')
+        photo_folder = 'saved'
+        for photo in info_photos:
+            now, max = 0, 0
+            for i in photo['items'][0]['attachments'][0]['photo']['sizes']:
+                now = i['width']
+                if now > max:
+                    max = now
+                    url = i['url']
+            if not os.path.exists(os.getcwd() + "/" + photo_folder + "/" + os.path.split(url)[1]):
+                urlretrieve(url, os.getcwd() + "/" + photo_folder + "/" + os.path.split(url)[1])
+        upload = vk_api.VkUpload(vk_private_session)
+        for element in os.listdir(path=os.getcwd() + '/' + photo_folder):
+            photo = upload.photo(
+                os.getcwd() + '/{0}/{1}'.format(photo_folder, element),
+                album_id=album_id,
+                group_id=group_id
+            )
+            attach = 'photo{}_{}'.format(photo[0]['owner_id'], photo[0]['id'])
+            all_tasks[seminar][task] = {"0": attach}
+            os.remove(path=os.getcwd() + '/{0}/{1}'.format(photo_folder, element))
+            response(event.user_id, "Добавлено, спасибо!")
+    except Exception as er:
+        print(er)
+        response(event.peer_id, 'Не удалось добавить фото в альбом группы. Произошла ошибка: {}'.format(er))
+    return all_tasks
+
+
 def main(all_tasks):
     """
-    в main идет бесконечный цикл, в котором запущен метод longpoll.listen(), который "прослушивает" сообщения, которые приходят боту.
+    в main запущен метод longpoll.listen(), который "прослушивает" сообщения, которые приходят боту.
     каждая стпуень проверят куда отправить запрос, и перенаправляет полученные данные в нужную функцию.
     конструкция try ... except использована для того, чтобы
-    во-первых, обойти ограничение, установленное методом longpoll.listen(). он запускает таймер и если никто не пишет в течение нескольких минут, то он выдает ошибку.
-    во-вторых, предотвратить поломку бота в случае, если кто-то из пользователей введет запрос, который будет некачественно обработан ботом и приведет к ошибке
+    во-первых, обойти ограничение, установленное методом longpoll.listen(). он запускает таймер и если
+    никто не пишет в течение нескольких минут, то он выдает ошибку.
+    во-вторых, предотвратить поломку бота в случае, если кто-то из пользователей введет запрос, который будет
+    некачественно обработан ботом и приведет к ошибке
     
     """
-    x = 1
     try:
-        while x == 1:
-            for event in longpoll.listen():
-                if event.type == vk_api.longpoll.VkEventType.MESSAGE_NEW and not event.from_me:
-                    if event.text == "stop23012001":
-                        x = 0
-                        break
-                    elif event.text.lower() == "хелп":
-                        ask_help(event.user_id)
-                    elif remember_users(event.user_id) == 0:
-                        user = vk_session.method("users.get", {"user_ids": event.user_id})
-                        response(event.user_id, "Привет, " + user[0]['first_name'] + "!")
-                        ask_help(event.user_id)
-                    elif "reply" in event.attachments.keys():
-                        accept_photo(event)
-                    elif "delete23012001" in event.text:
-                        delete_image(event.text, all_tasks)
-                    elif event.text.lower() == "все":
-                        print_all_tasks(all_tasks, event.user_id)
-                    elif event.text.lower() == "спасибо":
-                        response(event.user_id, "Пожалуйста)")
-                    else:
-                        analize_message(event, all_tasks)
+        for event in longpoll.listen():
+            if event.type == vk_api.longpoll.VkEventType.MESSAGE_NEW and not event.from_me:
+                if event.text == "stop23012001":
+                    break
+                elif event.text.lower() == "хелп":
+                    ask_help(event.user_id)
+                elif remember_users(event.user_id) == 0:
+                    user = vk_group_session.method("users.get", {"user_ids": event.user_id})
+                    response(event.user_id, "Привет, " + user[0]['first_name'] + "!")
+                    ask_help(event.user_id)
+                elif "reply" in event.attachments.keys():
+                    accept_photo(event)
+                elif "delete23012001" in event.text:
+                    delete_image(event.text, all_tasks)
+                elif event.text.lower() == "все":
+                    print_all_tasks(all_tasks, event.user_id)
+                elif event.text.lower() == "спасибо":
+                    response(event.user_id, "Пожалуйста)")
+                else:
+                    analize_message(event, all_tasks)
     except Exception as e:
         print("Error occured.")
         print(e)
